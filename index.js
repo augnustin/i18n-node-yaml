c71019dba62302e56d22f4253f9fab8f5cb4579e
Augustin Riedinger <augustin.riedinger@gmail.com>
2017-04-21 16:02:40 +0200

    fix

--- index.js
+++ index.js
@@ -13,22 +13,18 @@ module.exports = (options) => {
   let load = () => {
     return new Promise((resolveAll, rejectAll) => {
       fs.readdir(translationFolder, (err, files) => {
-        console.log(files);
         Promise.all(files.map(file => {
           let fileName = file.replace(new RegExp(path.extname(file) + '$'), '');
           return new Promise((resolve, reject) => {
             console.log(`${translationFolder}/${file}`);
             fs.readFile(`${translationFolder}/${file}`, 'utf8', (err, content) => {
-              console.log(content);
               resolve({[fileName]: yaml.safeLoad(content)});
             });
           });
         })).then((objects) => {
-          console.log(objects);
           translations = objects.reduce((result, object) => {
             return Object.assign(result, object);
           }, {});
-          console.log(translations);
           resolveAll(translations);
         });
       });
